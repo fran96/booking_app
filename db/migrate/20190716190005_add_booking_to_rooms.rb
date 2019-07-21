fffcecdcdf40f302c9ee29cc59ecfa95810cd986
micalleffran <micalleffran@gmail.com>
2019-07-21 22:44:53 +0200

    server pid in dockerfile,  deleted old migrations

--- db/migrate/20190716190005_add_booking_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddBookingToRooms < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :rooms, :booking, foreign_key: true
-  end
-end
--- db/migrate/20190716201349_remove_booking_from_rooms.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBookingFromRooms < ActiveRecord::Migration[5.2]
-  def change
-    remove_reference :rooms, :booking, foreign_key: true
-  end
-end
